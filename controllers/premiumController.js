d095588eb789089eb1c6bc82251be65d41ef6941
yash02500 <yashv0482@gmail.com>
2024-05-07 19:36:44 +0530

    Fixing bugs to make app production read

--- controllers/premiumController.js
+++ controllers/premiumController.js
@@ -46,35 +46,3 @@ exports.updateTransactionStatus = async(req, res) => {
         res.status(403).json({ error: err, message: 'Sometghing went wrong with update' })
     }
 };
-
-
-
-
-// const Razorpay = require('razorpay');
-// const Order = require('../models/orders');
-
-// const purchasepremium = async (req, res) => {
-//     try {
-//         var rzp = new Razorpay({
-//             key_id: process.env.RAZORPAY_KEY_ID,
-//             key_secret: process.env.RAZORPAY_KEY_SECRET
-//         })
-
-//         const amount = 2500;
-
-//         rzp.orders.create({amount, currency: "INR"}, (err, order) => {
-//             if (err) {
-//                 throw new Error(JSON.stringify(err));
-//             }
-
-//             req.user.createOrder({orderid: order.id, status: 'PENDING'}).then(() => {
-//                 return res.status(201).json({ order, key_id: rzp.key_id });
-//             }).catch(err => {
-//                 throw new Error(err)
-//             })
-//         })
-//     } catch(err) {
-//         console.log(err);
-//         res.status(403).json({message: 'Something went wrong', error: err})
-//     }
-// }
